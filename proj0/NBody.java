cf83798610bbd35b0e9c671f63be89d5f4f1fb20
agu <2374867967@qq.com>
2022-01-31 01:29:37 +0800

    finish project0

---
+++ proj0/NBody.java
@@ -0,0 +1,72 @@
+/**
+ * Simulate a sets of planets
+ * @author RicardoAGu
+ */
+public class NBody {
+    /**
+     * Read the radius of the system
+     * @param fileName the name of the .txt file which contains information of planets
+     */
+    public static double readRadius(String fileName){
+        In in = new In("./data/Planets.txt");
+        in.readInt();
+        return in.readDouble();
+    }
+
+    /**
+     * Read the information of planets and create corresponding Planet instances
+     * @param fileName the name of the .txt file which contains information of planets
+     */
+    public static Planet[] readPlanets(String fileName){
+        In in = new In(fileName);
+        int num = in.readInt();
+        Planet[] galaxy = new Planet[num];
+        in.readDouble();
+        for (int i = 0; i < num; i++) {
+            galaxy[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readString());
+        }
+        return galaxy;
+    }
+
+    /**
+     * Main method. Draw the universe in its starting position
+     */
+    public static void main(String[] args){
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        double radius = readRadius(filename);
+        Planet[] galaxy = readPlanets(filename);
+        double[] xForces = new double[galaxy.length];
+        double[] yForces = new double[galaxy.length];
+        StdDraw.setScale(-radius, radius);
+        StdDraw.clear();
+        StdDraw.picture(0,0, "images/starfield.jpg");
+        for (Planet p : galaxy) {
+            p.draw();
+        }
+        StdDraw.enableDoubleBuffering();
+        while(T > 0) {
+            T = T - dt;
+            for (int i = 0; i < galaxy.length; i++) {
+                xForces[i] = galaxy[i].calcNetForceExertedByX(galaxy);
+                yForces[i] = galaxy[i].calcNetForceExertedByY(galaxy);
+            }
+            StdDraw.clear();
+            StdDraw.picture(0,0, "images/starfield.jpg");
+            for (int i = 0; i < galaxy.length; i++) {
+                galaxy[i].update(dt, xForces[i], yForces[i]);
+                galaxy[i].draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+        }
+        StdOut.printf("%d\n", galaxy.length);
+        StdOut.printf("%.2e\n", radius);
+        for (Planet planet : galaxy) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    planet.xxPos, planet.yyPos, planet.xxVel,
+                    planet.yyVel, planet.mass, planet.imgFileName);
+        }
+    }
+}
---
+++ proj0/Planet.java
@@ -0,0 +1,126 @@
+/** definition of Planets
+ * @author RicardoAGu
+ */
+public class Planet {
+
+    public double xxPos, yyPos, xxVel, yyVel, mass;
+    public String imgFileName;
+
+    /** the first constructor
+     * @param xP Its current x position
+     * @param yP Its current y position
+     * @param xV Its current velocity in the x direction
+     * @param yV Its current velocity in the y direction
+     * @param m Its mass
+     * @param img The name of the file that corresponds
+     *            to the image that depicts the planet (for example, "jupiter.gif")
+     */
+    public Planet(double xP, double yP, double xV,
+                  double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    /** the first constructor
+     * @param p An existed planet instance
+     */
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
+    /** Calculate the distance between self and input planet
+     * @param p An existed planet instance
+     */
+    public double calcDistance(Planet p) {
+        return Math.sqrt(Math.pow(p.xxPos - xxPos, 2) + Math.pow(p.yyPos - yyPos, 2));
+    }
+
+    /** Calculate the force exerted on this planet by the given planet
+     * @param p An existed planet instance
+     */
+    public double calcForceExertedBy(Planet p){
+        return 6.67 * Math.pow(10, -11) * mass * p.mass / calcDistance(p) / calcDistance(p);
+    }
+
+    /** Calculate the force exerted on this planet in the X direction by the given planet
+     * @param p An existed planet instance
+     */
+    public double calcForceExertedByX(Planet p) {
+        return calcForceExertedBy(p) * (p.xxPos - xxPos) / calcDistance(p);
+    }
+
+    /** Calculate the force exerted on this planet in the Y direction by the given planet
+     * @param p An existed planet instance
+     */
+    public double calcForceExertedByY(Planet p) {
+        return calcForceExertedBy(p) * (p.yyPos - yyPos) / calcDistance(p);
+    }
+
+    /** Check if the given planet is just this planet
+     * @param p An existed planet instance
+     */
+    public boolean equals(Planet p){
+        if (imgFileName != p.imgFileName) {
+            return false;
+        } else if (xxPos - p.xxPos + yyPos - p.yyPos + xxVel - p.xxVel + yyVel - p.yyVel + mass - p.mass != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    /** Calculate the net force exerted on this planet in the X direction by all the given planets
+     * @param galaxy All existed planet instances
+     */
+    public double calcNetForceExertedByX(Planet[] galaxy) {
+        double netForceX = 0.0;
+        for (Planet p : galaxy) {
+            if (! equals(p)) {
+                netForceX += calcForceExertedByX(p);
+            }
+        }
+        return netForceX;
+    }
+
+    /** Calculate the net force exerted on this planet in the X direction by all the given planets
+     * @param galaxy All existed planet instances
+     */
+    public double calcNetForceExertedByY(Planet[] galaxy) {
+        double netForceY = 0.0;
+        for (Planet p : galaxy) {
+            if (! equals(p)) {
+                netForceY += calcForceExertedByY(p);
+            }
+        }
+        return netForceY;
+    }
+
+    /** Calculate the change in the planet’s velocity and position during time t caused by forces
+     * @param t A small period of time
+     * @param forceX The net force exerted on this planet in the X direction
+     * @param forceY The net force exerted on this planet in the Y direction
+     */
+    public void update(double t, double forceX, double forceY) {
+        double aX, aY;
+        aX = forceX / mass;
+        aY = forceY / mass;
+        xxVel += aX * t;
+        yyVel += aY * t;
+        xxPos += xxVel * t;
+        yyPos += yyVel * t;
+    }
+
+    /** Draw this planet
+     */
+    public void draw() {
+        StdDraw.picture(xxPos, yyPos, "images/" + imgFileName);
+    }
+}
